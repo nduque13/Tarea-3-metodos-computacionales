043abfa34729487e7f8a1047ede0006ae4ad4567
Nicolas Duque Valenzuela <UANDES\n.duque13@fisicaez12219l.ad.uniandes.edu.co>
2018-10-30 10:37:44 -0500

    Crea listas para almacenar

--- PCR.py
+++ PCR.py
@@ -7,3 +7,15 @@ from numpy.linalg import eig
 
 #Importa los datos de WDBC.dat con genfromtxt
 datos = np.genfromtxt('WDBC.dat', dtype='str')
+
+
+
+#Creo dos listas vacias que me almacenen los numeros y las letras, debido a la presencia de el campo con la letra M o B en los datos .dat
+
+
+L = []
+
+
+
+
+N = []
