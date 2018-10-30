03592a79489e8bdc334895b3d469bc2160e02741
Nicolas Duque Valenzuela <UANDES\n.duque13@fisicaez12219l.ad.uniandes.edu.co>
2018-10-30 10:25:25 -0500

    Comienza el segundo punto, importa librerias

---
+++ PCR.py
@@ -0,0 +1,6 @@
+##Comienza el punto 2 de la tarea 3 de metodos computacionales PCA-PCR
+
+###IMPORTA LIBRERIAS NECESARIAS
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.linalg import eig
