3b2ec69fe29de1fb9402173764e2df04b9cf481c
gitGabrielGonzalez <ge.gonzalez13@uniandes.edu.co>
2018-10-31 09:18:25 -0500

    Avance de el 2d, ya lei la imagen y la grafique, voy colgado

--- Fourier2D.py
+++ Fourier2D.py
@@ -1,8 +1,44 @@
-##Comienza el punto 4 de la tarea 3 de metodos computacionales PCA-PCR
+##Comienza el punto 4 de la tarea 3 de metodos computacionales Fourier en 2d
 
-###IMPORTA LIBRERIAS NECESARIAS
+
+###IMPORTA LIBRERIAS NECESARIAS incluyendo la lectura de imagenes de matplotlib (imread)
+
+
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.image import imread
+from scipy import fft, fftfreq, fft2, ifft, ifft2
 
 
-##cambio de prueba
+# Almacene la imagen arbol.png en una arreglo de numpy
+
+miarbolpapa = imread('arbol.PNG')
+
+
+#Usando los paquetes de scipy, realice la transformada de Fourier de la imagen. Eligiendo
+#una escala apropiada, haga una gráfica de dicha transformada y guárdela sin mostrarla en
+#ApellidoNombre_FT2D.pdf.
+
+##Realiza la transformada en 2-D utilizando fft
+
+transforma = np.fft.fft2(miarbolpapa)
+##plotea la transformada que acaba de realizar
+
+plt.figure()
+#utiliza el paquete imshow para leer la imagen
+
+
+plt.imshow(np.abs(transforma))
+
+plt.xlabel('X')
+plt.ylabel('Y')
+fig.savefig('DuqueNicolas_FT2D.pdf')
+
+
+
+
+
+
+
+
+
