71cee8683666bc05746cf551634a07057f588a06
Nicolas Duque Valenzuela <UANDES\n.duque13@fisicaez12219l.ad.uniandes.edu.co>
2018-10-31 18:54:29 -0500

    ya casi

--- Fourier2D.py
+++ Fourier2D.py
@@ -12,7 +12,7 @@ from scipy.fftpack import fft, fftfreq, ifft, fft2, ifft2
 
 # Almacene la imagen arbol.png en una arreglo de numpy
 
-miarbolpapa = imread('arbol.png')
+miarbolpapa = imread('arbol.PNG')
 
 
 #Usando los paquetes de scipy, realice la transformada de Fourier de la imagen. Eligiendo
@@ -21,7 +21,7 @@ miarbolpapa = imread('arbol.png')
 
 ##Realiza la transformada en 2-D utilizando fft
 
-transforma = fft2(miarbolpapa)
+transforma = np.fft.fft2(miarbolpapa)
 ##plotea la transformada que acaba de realizar
 
 plt.figure()
@@ -35,28 +35,39 @@ plt.ylabel('Y')
 plt.savefig('DuqueNicolas_FT2D.pdf')
 
 ##se determina que un filtro bueno es alrededor de 3450 o 3400 o 3500, alguno de esos, por lo que en vez de buscar esa amplitud con un for, se utiliza la funcion de numpy np.where para que busque las amplitudes mayores a 3500 y las elimina.
-mayoresque3400 = np.where(transforma > 3400)
+
+
+#busca en donde hay amplitudes mayores a 3500 y las elimina
+
+mayoresque3500 = np.where(transforma > 3500)
 
 
 
-transforma[mayoresque3400] = 0
+transforma[mayoresque3500] = 0
+scalita = np.log(np.abs(transforma))
+#tiene que usar escala logaritmica entonces para eso utiliza
+
+
+
 
 ##Grafique la transformada de Fourier despues del proceso de filtrado, esta vez en escala
 #LogNorm y guarde dicha grafica sin mostrarla en ApellidoNombre_FT2D_filtrada.pdf.
 
 plt.figure()
 
-
-#tiene que usar escala logaritmica entonces para eso utiliza
-
-escalalog = np.log(np.abs(transforma))
-plt.imshow(escalalog)
-
+plt.imshow(scalita)
 plt.grid()
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.colorbar()
+
 plt.savefig('DuqueNicolas_FT2D_filtrada.pdf')
+
+
+#Haga la transformada de Fourier inversa y grafique la imagen filtrada. Verifique que su
+#filtro elimina el ruido periodico y guarde dicha imagen sin mostrarla en
+#ApellidoNombre_Imagen_filtrada.pdf.
+
 
 
 
