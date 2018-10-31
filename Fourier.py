f18628a75ad062011fe0d21491b2e6edd5ee27dc
Nicolas Duque Valenzuela <UANDES\n.duque13@fisicaez12219l.ad.uniandes.edu.co>
2018-10-31 17:11:41 -0500

    Fourier

--- Fourier.py
+++ Fourier.py
@@ -16,7 +16,7 @@ losincompletos = np.genfromtxt('incompletos.dat')
 
 ##Como en signal.dat hay entradas que no son numeros, se aplica slicing para que no afecten el ejercicio
 
-lasenal = lasenal[:,[0,-1]]
+lasenal = lasenal[:, 2]
 
 
 ##Hace una grafica de senal y la guarda sin mostrarla como es indicado
@@ -29,30 +29,37 @@ plt.savefig("DuqueNicolas_signal.pdf")
 
 ###Hace implementacion propia de la transformada de fourier de los datos
 lensen = len(lasenal)
-print (lensen)
+
 
 N = lensen
 
-for n in range(N):
-	suma = 0.0
-	for i in range(N):
-		suma +=  lasenal[i]*(np.exp(-1j*2*np.pi*i*(n/N)))
-	print(suma)
-suma = np.array(suma)
-print("La transformada de fourier es:", suma)
 
-prueba = fft(lasenal)
-print("Teorico es------------------")
-print(prueba)
+def fourier(lasenal):
+	fourier = []
+	for n in range(N):
+		suma = 0.0
+		for i in range(N):
+			suma +=  (lasenal[i])*(np.exp(-1j*2*np.pi*i*(n/N)))
+			fourier.append(suma)
+	return fourier
+porfinfourier = fourier(lasenal)
+print(porfinfourier)
+print("La transformada de fourier es:", porfinfourier)
 
 
-##grafica la transformada de fourier encontrada por implementacion propia y la transformada de fourier con paquete LO HAGO POR SABER COMO SON LAS GRAFICAS utiliza el paquete de fftfreq
 
-dt = 1.0/N
-freqsenal = fftfreq(N,dt)
+##grafica la transformada de fourier encontrada por implementacion propia y la transformada de fourier con paquete LO HAGO POR SABER COMO SON LAS GRAFICAS utiliza el paquete de fftfreq NO ME FUNCIONA Y ME GRAFICA LO QUE NO ES, por lo que dejo comentado como me daria con mi implementacion y la grafico con los paquetes de scipy
 
-plt.plot(freqsenal,prueba)
-plt.show()
+longfouri = len(porfinfourier) 
+NN = longfouri
+dt = 1.0/NN
+freqsenal = fftfreq(NN,dt)
+
+#plt.plot(freqsenal,porfinfourier)
+#plt.show()
+
+trans = fft(lasenal)
+
 
 
 
